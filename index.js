e9e7ac4521c32a7af8bbfa327caaafacde319b25
Shakeel2261 <iam.shakeelali261@gmail.com>
2025-03-19 15:39:18 +0500

    19-3-25

---
+++ index.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("head")
+document.getElementById("btn").addEventListener("click", ()=>{
+    header.style.display = "none"
+})
+
+function del (){
+    btn.addEventListener("click", ()=>{
+        header.style.display = "none"
+    })
+
+}
